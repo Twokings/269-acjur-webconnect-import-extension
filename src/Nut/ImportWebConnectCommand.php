bdbc981b6a68aeb72a433eabaffcc6476cd475c3
nestordedios <nestordedios@outlook.es>
2018-04-03 16:58:03 +0200

    Add ImportWebConnectCommand class

---
+++ src/Nut/ImportWebConnectCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bolt\Extension\TwoKings\ImportWebConnect\Nut;
+
+use Bolt\Nut\BaseCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Nut command for the ImportWebConnect extension.
+ *
+ * @author Néstor de Dios Fernández <nestor@twokings.nl>
+ */
+class ImportWebConnect extends BaseCommand
+{
+}
