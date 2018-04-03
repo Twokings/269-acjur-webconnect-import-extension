82c45d0618010f657f4f679aefdf1e1f8f060ae7
nestordedios <nestordedios@outlook.es>
2018-04-03 16:51:16 +0200

    Add Provider class

---
+++ src/Provider/ImportWebConnectProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\Extension\TwoKings\ImportWebConnect\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * ImportWebConnect service provider.
+ *
+ * @author Néstor de Dios Fernández <nestor@twokings.nl>
+ */
+class ImportWebConnectProvider implements ServiceProviderInterface
+{
+}
