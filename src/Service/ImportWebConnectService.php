c2374d6d856670d910492aa98518016639f772a6
nestordedios <nestordedios@outlook.es>
2018-04-03 16:51:45 +0200

    Add Service class

---
+++ src/Service/ImportWebConnectService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\Extension\TwoKings\ImportWebConnect\Service;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * ImportWebConnect service.
+ *
+ * @author Néstor de Dios Fernández <nestor@twokings.nl>
+ */
+class ImportWebConnectService implements ServiceProviderInterface
+{
+}
