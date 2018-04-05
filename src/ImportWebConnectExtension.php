041bc3196c7f718a4c7a3d45193eaaae87bc1abc
nestordedios <nestordedios@outlook.es>
2018-04-05 10:17:08 +0200

    Add a menu entry in he backend

--- src/ImportWebConnectExtension.php
+++ src/ImportWebConnectExtension.php
@@ -3,12 +3,41 @@
 namespace Bolt\Extension\TwoKings\ImportWebConnect;
 
 use Bolt\Extension\SimpleExtension;
+use Bolt\Menu\MenuEntry;
 
 /**
- * ExtensionName extension class.
+ * ImportWebConnect extension class.
  *
- * @author Your Name <you@example.com>
+ * @author Néstor de Dios Fernández <nestor@twokings.nl>
  */
 class ImportWebConnectExtension extends SimpleExtension
 {
+    /**
+     * {@inheritdoc}
+     *
+     * Extending the backend menu:
+     *
+     * You can provide new Backend sites with their own menu option and template.
+     *
+     * Here we will add a new route to the system and register the menu option in the backend.
+     *
+     * You'll find the new menu option under "Extras".
+     */
+    protected function registerMenuEntries()
+    {
+        /*
+         * Define a menu entry object and register it:
+         *   - Route http://example.com/bolt/extensions/importwebconnect
+         *   - Menu label 'Import WebConnect'
+         *   - Menu icon a Font Awesome small child
+         *   - Required Bolt permissions 'settings'
+         */
+        $adminMenuEntry = (new MenuEntry('importwebconnect-backend-page', 'importwebconnect'))
+            ->setLabel('Import WebConnect')
+            ->setIcon('fa:child')
+            ->setPermission('extensions')
+        ;
+
+        return [$adminMenuEntry];
+    }
 }
