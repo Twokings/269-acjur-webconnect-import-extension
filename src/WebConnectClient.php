c932cb55abf75ff99e2014fc59e8ddea21d277a1
nestordedios <nestordedios@outlook.es>
2018-04-05 10:12:31 +0200

    Add WebConnectClient class

---
+++ src/WebConnectClient.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bolt\Extension\TwoKings\ImportWebConnect;
+
+/**
+ * WebConnect Client class.
+ *
+ * @author Néstor de Dios Fernández <nestor@twokings.nl>
+ */
+class WebConnectClient
+{
+
+}
