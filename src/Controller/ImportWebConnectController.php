1d0a1b109f5612889328b61578a4ace4ebc2f313
nestordedios <nestordedios@outlook.es>
2018-04-05 10:32:52 +0200

    Add ImportWebConnectController class and import route

---
+++ src/Controller/ImportWebConnectController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Bolt\Extension\TwoKings\ImportWebConnect\Controller;
+
+use Bolt\Controller\Base;
+use Silex\Application;
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ *
+ *
+ * @author Néstor de Dios Fernández <nestor@twokings.nl>
+ */
+class NotarisController extends Base
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function addRoutes(ControllerCollection $ctr)
+    {
+        $ctr
+            ->match('/extensions/importwebconnect', [$this, 'importwebconnectBackendPage'])
+            ->before([$this, 'before'])
+            ->bind('webconnect.import.get');
+
+        return $ctr;
+    }
+
+    /**
+     * Check if the current user is logged in.
+     *
+     * @param Request     $request
+     * @param Application $app
+     */
+    public function before(Request $request, Application $app)
+    {
+        $token = $app['session']->get('authentication', false);
+
+        if (! $token) {
+            return $this->redirectToRoute('dashboard');
+        }
+    }
+
+    /**
+     *
+     *
+     * @param Application $app
+     * @param Request     $request
+     */
+    public function importwebconnectBackendPage(Application $app, Request $request)
+    {
+
+    }
+}
